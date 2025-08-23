1e6521d106bdbc026f207d3687b3eda9bcf69aa8
ryuukitomi <longhuidengmei@gmail.com>
2025-08-23 17:23:40 +0900

    アイテム

--- Assets/Scenes/script/item/ItemSystem.cs
+++ Assets/Scenes/script/item/ItemSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemSystem : MonoBehaviour
@@ -7,12 +8,21 @@ public class ItemSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Iitem item = collision.gameObject.GetComponent<Iitem>();
+        if(item != null)
+        {
+            item.ActivateEffect(gameObject);
+        }
     }
 }
