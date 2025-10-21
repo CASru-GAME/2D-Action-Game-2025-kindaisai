b2e80c0ed6e8c7b4a6e80a5fe2e82c73b1ae1a2f
ryuukitomi <longhuidengmei@gmail.com>
2025-10-21 16:13:53 +0900

    滑る床

---
+++ Assets/Scripts/Gimmick/IceObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerDataStore playerDataStore = collision.gameObject.GetComponent<PlayerDataStore>();
+        if (playerDataStore != null)
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            rb.velocity += new Vector2(Mathf.Max(1f,rb.velocity.x/2f),0);
+        }
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        PlayerDataStore playerDataStore = collision.gameObject.GetComponent<PlayerDataStore>();
+        if (playerDataStore != null)
+            playerDataStore.PlayerController2D.isOnIce = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        PlayerDataStore playerDataStore = collision.gameObject.GetComponent<PlayerDataStore>();
+        if (playerDataStore != null)
+            playerDataStore.PlayerController2D.isOnIce = false;
+    }
+}
--- Assets/Scripts/Player/PlayerController2D.cs
+++ Assets/Scripts/Player/PlayerController2D.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
@@ -5,6 +7,11 @@ public class PlayerController2D : MonoBehaviour
     [Header("移動設定")]
     [SerializeField] private float moveSpeed = 5f;    // 通常移動速度
     [SerializeField] private float dashSpeed = 8f;    // ダッシュ時の速度
+    [SerializeField] private float moveAcceleration;
+    [SerializeField] private float dashAcceleration;
+    public bool isOnIce;
+    [SerializeField] float iceForward;//氷の進行方向へのすべりやすさ
+    [SerializeField] float iceBackward;//氷の逆方向へのすべりやすさ
 
     [Header("ジャンプ設定")]
     [SerializeField] private float jumpForce = 7f;           // ジャンプ初速
@@ -45,10 +52,32 @@ public class PlayerController2D : MonoBehaviour
         }
         // 左右移動
         float moveInput = Input.GetAxisRaw("Horizontal");
-        float speed = (Input.GetKey(KeyCode.LeftShift)) ? dashSpeed : moveSpeed;
-        rb.velocity = new Vector2((isReverse ? -1 : 1) * moveInput * speed, rb.velocity.y);
+        float accelerationSpeed;
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            accelerationSpeed = SetAcceleration(moveInput, dashAcceleration);
 
+            if (Mathf.Abs(rb.velocity.x + (isReverse ? -1 : 1) * accelerationSpeed) <= dashSpeed)
+                rb.velocity += new Vector2((isReverse ? -1 : 1) * accelerationSpeed, 0);
+            else if (Mathf.Abs(rb.velocity.x) > dashSpeed)
+            Stop();
+        }
+        else
+        {
+            accelerationSpeed = SetAcceleration(moveInput, moveAcceleration);
+
+            if (Mathf.Abs(rb.velocity.x + (isReverse ? -1 : 1) * accelerationSpeed) <= moveSpeed)
+                rb.velocity += new Vector2((isReverse ? -1 : 1) * accelerationSpeed, 0);
+            else if (rb.velocity.x * moveInput < 0)
+                rb.velocity += new Vector2((isReverse ? -1 : 1) * accelerationSpeed, 0);
+            else if (Mathf.Abs(rb.velocity.x) > moveSpeed)
+                Stop();
+
+        }
+        if (moveInput == 0)
+            Stop();
+        
         // ジャンプ開始（ボタンを押した瞬間）
         if (Input.GetButtonDown("Jump"))
         {
@@ -121,5 +150,36 @@ public class PlayerController2D : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
+    }
+
+    void Stop()
+    {
+        if (rb.velocity.x > 0)
+        {
+            rb.velocity += new Vector2(isOnIce ? -moveAcceleration / 4f : -moveAcceleration / 2f, 0);
+            if (rb.velocity.x < 0)
+                rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            rb.velocity += new Vector2(isOnIce ? moveAcceleration / 4f : moveAcceleration / 2f, 0);
+            if (rb.velocity.x > 0)
+                rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+    }
+    float SetAcceleration(float moveInput,float acceleration)
+    {
+        float accelerationSpeed;
+        if (isOnIce)
+        {
+            if (moveInput * rb.velocity.x > 0)
+                accelerationSpeed = moveInput * acceleration * iceForward;
+            else
+                accelerationSpeed = moveInput * acceleration * iceBackward;
+        }
+        else
+        accelerationSpeed = moveInput * acceleration;
+        
+        return accelerationSpeed;
     }
 }
