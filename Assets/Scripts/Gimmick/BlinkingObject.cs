a8c4aedb0323f0a58a31162d60913b33f7faabdd
ryuukitomi <longhuidengmei@gmail.com>
2025-10-31 07:20:53 +0900

    点滅床の置き換え

--- Assets/Scripts/Gimmick/BlinkingObject.cs
+++ Assets/Scripts/Gimmick/BlinkingObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BlinkingObject : MonoBehaviour
 {
@@ -9,15 +10,44 @@ public class BlinkingObject : MonoBehaviour
     [Header("消えている時間（秒）")]
     public float invisibleTime = 2f; // 消えている時間
 
-    private Renderer rend;
-    private Collider2D col;
+    private SpriteRenderer rend;
+    private TilemapCollider2D col;
+    [SerializeField] Tilemap tilemap;
+
+    [SerializeField] TileBase VisibleTile,InvisibleTile;
+    
+    [SerializeField] bool isVisible;
+    [SerializeField] Vector3Int[] Position;
+    float time;
 
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        col = GetComponent<Collider2D>();
-        StartCoroutine(TogglePlatform());
+        rend = GetComponent<SpriteRenderer>();
+        col = GetComponent<TilemapCollider2D>();
+        //StartCoroutine(TogglePlatform());
+        time = visibleTime;
+
+        col.enabled = isVisible;
+        foreach(var pos in Position)
+        if(isVisible) tilemap.SetTile(pos,VisibleTile);
+        else tilemap.SetTile(pos,InvisibleTile);
+    }
+
+    void Update()
+    {   
+        time -= Time.deltaTime;
+        if(time <= 0f)
+        {
+            isVisible = !isVisible;
+            time = visibleTime;
+
+            col.enabled = isVisible;
+
+            foreach(var pos in Position)
+            if(isVisible) tilemap.SetTile(pos,VisibleTile);
+            else tilemap.SetTile(pos,InvisibleTile);
+        }
     }
 
     System.Collections.IEnumerator TogglePlatform()
