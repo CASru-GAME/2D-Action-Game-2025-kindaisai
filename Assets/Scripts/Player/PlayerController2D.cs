435464d0b6b90f3efbadb962c2f9481af66ab174
ryuukitomi <longhuidengmei@gmail.com>
2025-11-01 13:51:23 +0900

    ww

--- Assets/Scripts/Player/PlayerController2D.cs
+++ Assets/Scripts/Player/PlayerController2D.cs
@@ -69,7 +69,7 @@ public class PlayerController2D : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
-    void Update()
+    void FixedUpdate()
     {           
         //カメラ追従
         PlayerScreenPos = Camera.main.WorldToScreenPoint(transform.position);
